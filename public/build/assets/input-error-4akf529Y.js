5303b3cd09cb573d0da50cd49888c2c5cb747c85
gabrielmoraes102roo@gmail.com <96112719Gg.>
2025-09-10 19:35:38 -0400

    Atualização

---
+++ public/build/assets/input-error-4akf529Y.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-B6UBdJlz.js";import{c as o}from"./button-CLsutRgt.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
